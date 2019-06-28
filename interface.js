38a83aef0bf5782ec9bc9a2bb56bb68f2812d409
Student <student@MacBook-Pro-5.local>
2019-06-28 16:43:32 +0100

    all buttons linked and working fine

--- interface.js
+++ interface.js
@@ -7,17 +7,37 @@ $('#temperature').text(thermostat.currentTemp());
 
 $('#temperature-up').click(function() {
   thermostat.tempu();
-  $('#temperature').text(thermostat.currentTemp());
+  // $('#temperature').text(thermostat.currentTemp());
+  updateTemperature();
 });
 
 $('#temperature-down').click(function() {
   thermostat.tempd();
-  $('#temperature').text(thermostat.currentTemp());
+  updateTemperature();
 });
 
 $('#temperature-reset').click(function(){
   thermostat.reset();
-  $('#temperature').text(thermostat.currentTemp());
-})
+  updateTemperature();
+});
 
+$('#power-saving-status').text(thermostat.powerSavingStatus());
+
+$('#powersaving-on').click(function(){
+  thermostat.switchon();
+$('#power-saving-status').text(thermostat.powerSavingStatus());
+updateTemperature();
+
+});
+
+$('#powersaving-off').click(function(){
+  thermostat.switchoff();
+$('#power-saving-status').text(thermostat.powerSavingStatus());
+updateTemperature();
+});
+
+function updateTemperature() {
+  $('#temperature').text(thermostat.temperature);
+  $('#temperature').attr('class', thermostat.energyUsage());
+}
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,10 +2,28 @@
 
 function Thermostat() {
   this.temperature = 20;
-  this.mintemp = 10;
+  this.minTemp = 10;
   this.maxTemperature = 25;
   this.powerSavingMode = true;
 }
+
+Thermostat.prototype.powerSavingStatus = function() {
+  if(this.powerSavingMode){
+    return "on";
+  } else {
+    return "off";
+  }
+};
+
+Thermostat.prototype.switchon = function(){
+    this.powerSavingMode = true;
+    this.maxTemperature = 25;
+  };
+Thermostat.prototype.switchoff = function() {
+  this.powerSavingMode = false;
+  this.maxTemperature = 32;
+};
+
 
 Thermostat.prototype.currentTemp = function() {
   return this.temperature;
@@ -16,19 +34,20 @@ Thermostat.prototype.tempu = function(){
       this.temperature ++;
   }};
 
-Thermostat.prototype.tempd = function(){
-  if(this.temperature > this.mintemp){
+Thermostat.prototype.tempd = function() {
+  if(this.temperature > this.minTemp){
     this.temperature --;
   }};
 
-Thermostat.prototype.powerSavingSwitch = function(){
-  if(this.powerSavingMode) {
-    this.powerSavingMode = false;
-    this.maxTemperature = 32;
-  } else {
-    this.powerSavingmode = true;
-    this.maxTemperature = 25;
-  }};
-Thermostat.prototype.reset = function(){
+
+Thermostat.prototype.reset = function() {
   this.temperature = 20
 };
+
+Thermostat.prototype.energyUsage = function(){
+  if(this.temperature < 18){
+    return "low-usage"
+  } else if(this.temperature < 25 && this.temperature >= 18){
+    return "medium-usage"
+  } else { return "high-usage" }
+}
